390c9a72d0f426975850ccc60c9aea849cf3ac35
Louis Ball <louis.ball@me.com>
2015-12-20 16:15:17 -0800

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "/books" => "books#{index}", as: 'books'
-  get "/books/:id" => "books#show", as: 'book'
+#  get "/books" => "books#{index}", as: 'books'
+ # get "/books/:id" => "books#show", as: 'book'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +55,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+#end
